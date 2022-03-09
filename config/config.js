9e7c2b73fb130c4fe9e790a48f24c05c4a2e7c51
João Felipe P <jf.pelliccione@gmail.com>
2022-03-09 10:55:16 -0300

    Botando banco de dados para nuvem.

--- config/config.js
+++ config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.HOSTNAME,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: 'mysql',
   },
   test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
     database: 'izi_tech_test',
-    host: process.env.HOSTNAME,
+    host: process.env.HOST,
     dialect: 'mysql',
   },
   production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
     database: 'izi_tech_production',
-    host: process.env.HOSTNAME,
+    host: process.env.HOST,
     dialect: 'mysql',
   },
 };
