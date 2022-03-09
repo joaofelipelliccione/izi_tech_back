dd6737cb590f8e123255b57e24c28a3f9260a41c
João Felipe P <jf.pelliccione@gmail.com>
2022-03-09 17:37:27 -0300

    Realizando pequenos acertos.

--- api/app.js
+++ api/app.js
@@ -19,7 +19,6 @@ const errorMw = require('./middlewares/errorMw');
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Authorization'],
 }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
