861b66e49092b7d7b8c5073bc61de7cc1682b7f7
João Felipe P <jf.pelliccione@gmail.com>
2022-03-09 12:11:58 -0300

    Alterando dados do banco.

--- config/config.js
+++ config/config.js
@@ -2,22 +2,22 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    username: process.env.JAWS_DB_USERNAME,
+    password: process.env.JAWS_DB_PASSWORD,
+    database: process.env.JAWS_DB_NAME,
     host: process.env.HOST,
     dialect: 'mysql',
   },
   test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: process.env.JAWS_DB_USERNAME,
+    password: process.env.JAWS_DB_PASSWORD,
     database: 'izi_tech_test',
     host: process.env.HOST,
     dialect: 'mysql',
   },
   production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: process.env.JAWS_DB_USERNAME,
+    password: process.env.JAWS_DB_PASSWORD,
     database: 'izi_tech_production',
     host: process.env.HOST,
     dialect: 'mysql',
