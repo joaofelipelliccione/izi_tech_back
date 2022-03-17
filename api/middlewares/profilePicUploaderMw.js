61a298a6023d3daa48f347bb2ab54210724fc195
João Felipe P <jf.pelliccione@gmail.com>
2022-03-17 18:39:59 -0300

    Estruturando Google Cloud Storage.

--- api/middlewares/profilePicUploaderMw.js
+++ api/middlewares/profilePicUploaderMw.js
@@ -1,13 +1,31 @@
+// const multer = require('multer');
+
+// const storage = multer.diskStorage({
+//   destination(_req, _file, cb) {
+//     cb(null, './api/uploads/avatars');
+//   },
+//   filename(req, file, cb) {
+//     const { id } = req.params;
+//     cb(null, `${id}.${file.mimetype.split('/')[1]}`);
+//   },
+// });
+
+// module.exports = multer({ storage });
+
+require('dotenv').config();
 const multer = require('multer');
+const multerGoogleStorage = require('multer-google-storage');
 
-const storage = multer.diskStorage({
-  destination(_req, _file, cb) {
-    cb(null, './api/uploads/avatars');
-  },
-  filename(req, file, cb) {
-    const { id } = req.params;
-    cb(null, `${id}.${file.mimetype.split('/')[1]}`);
-  },
+module.exports = multer({
+  storage: multerGoogleStorage.storageEngine({
+    autoRetry: true,
+    maxRetries: 1,
+    bucket: process.env.GCS_BUCKET,
+    projectId: process.env.GCLOUD_PROJECT,
+    keyFilename: process.env.GCS_KEYFILE,
+    filename: (req, file, cb) => {
+      const { id } = req.params;
+      cb(null, `${id}.${file.mimetype.split('/')[1]}`);
+    },
+  }),
 });
-
-module.exports = multer({ storage });
