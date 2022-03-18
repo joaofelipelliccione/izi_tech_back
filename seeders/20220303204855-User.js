77ce2eb16ead4d120541974b2565dbb587d25892
João Felipe P <jf.pelliccione@gmail.com>
2022-03-18 14:21:35 -0300

    Tentando fazer funcionar.

--- seeders/20220303204855-User.js
+++ seeders/20220303204855-User.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable sonarjs/no-duplicate-string */
 /* eslint-disable max-lines-per-function */
 
@@ -12,7 +13,7 @@ module.exports = {
         userBirthday: '1998-10-23',
         userCPF: '180.559.177-01',
         userCellphone: '(21) 97257-4272',
-        userPicture: './registeredUsersPictures/jfProfilePic.jpeg',
+        userPicture: 'https://storage.googleapis.com/gcsb-izi-tech-profile-pictures/1_d1ed6318-16db-42c1-9bb3-9166aceed20d.jpeg',
         dateOfRegister: '2020-08-05',
         userAddressId: 1,
       },
@@ -24,7 +25,7 @@ module.exports = {
         userBirthday: '1998-12-12',
         userCPF: '180.143.187-65',
         userCellphone: '(21) 99945-1971',
-        userPicture: './registeredUsersPictures/luProfilePic.jpeg',
+        userPicture: 'https://storage.googleapis.com/gcsb-izi-tech-profile-pictures/2_1b3e75f7-7501-4520-9f15-dccdd4ff004a.jpeg',
         dateOfRegister: '2020-10-17',
         userAddressId: 2,
       },
@@ -36,7 +37,7 @@ module.exports = {
         userBirthday: '1994-09-02',
         userCPF: '154.328.037-47',
         userCellphone: '(21) 99519-4210',
-        userPicture: './registeredUsersPictures/viviProfilePic.jpeg',
+        userPicture: 'https://storage.googleapis.com/gcsb-izi-tech-profile-pictures/3_1812925f-b388-4a10-b950-8cd803740871.jpeg',
         dateOfRegister: '2021-01-04',
         userAddressId: 3,
       },
@@ -48,7 +49,7 @@ module.exports = {
         userBirthday: '1960-09-17',
         userCPF: '667.446.877-01',
         userCellphone: '(21) 99917-1710',
-        userPicture: './registeredUsersPictures/andreProfilePic.jpeg',
+        userPicture: 'https://storage.googleapis.com/gcsb-izi-tech-profile-pictures/4_8bfcd3ed-3ba4-4560-be5b-e048bf8f7ec9.jpeg',
         dateOfRegister: '2021-02-15',
         userAddressId: 4,
       },
