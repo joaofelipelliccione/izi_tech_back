64ca0a9cde5d18b735644d365eb1787668c6492f
João Felipe P <jf.pelliccione@gmail.com>
2022-03-22 14:56:01 -0300

    Conclusão da estrutura do JSON de produtos.

--- api/services/PublishedProductsService.js
+++ api/services/PublishedProductsService.js
@@ -1,15 +1,52 @@
+/* eslint-disable max-lines-per-function */
+
 const {
   PublishedProducts,
-  // User,
-  // ProductType,
-  // ProductLine,
-  // ProductTopCategory,
-  // ProductCondition,
-  // InfoFromCep
+  ProductCondition,
+  ProductType,
+  ProductLine,
+  ProductTopCategory,
+  ProductPictures,
+  InfoFromCep,
+  User,
 } = require('../../models/index');
 
 const findAll = async () => {
   const publishedProducts = await PublishedProducts.findAll({
+    attributes: { exclude: ['productConditionId', 'productTypeId', 'infoFromCepId', 'userId'] },
+    include: [
+      {
+        model: ProductCondition,
+        as: 'productCondition',
+      },
+      {
+        model: ProductType,
+        as: 'productType',
+        include: [{
+          model: ProductLine,
+          as: 'productLine',
+          include: [{ model: ProductTopCategory, as: 'topCategory' }],
+          attributes: { exclude: ['topCategoryId'] },
+        }],
+        attributes: { exclude: ['productLineId'] },
+      },
+      {
+        model: ProductPictures,
+        as: 'productPictures',
+        attributes: { exclude: ['productPictureId', 'productId'] },
+      },
+      {
+        model: InfoFromCep,
+        as: 'infoFromCep',
+      },
+      {
+        model: User,
+        as: 'userInfo',
+        attributes: {
+          exclude: ['userPassword', 'userBirthday', 'userCPF', 'userPicture', 'userAddressId'],
+        },
+      },
+    ],
   });
   return publishedProducts;
 };
