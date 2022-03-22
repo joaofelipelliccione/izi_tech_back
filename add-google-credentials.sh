583a0ac9c65dcab9cecff01ab7f290f7992faffe
João Felipe P <jf.pelliccione@gmail.com>
2022-03-22 18:32:44 -0300

    Criando outro bucket.

--- add-google-credentials.sh
+++ add-google-credentials.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-echo "Generating google-credentials.json from Heroku environment variable"
+echo "Generating google-credentials.json and google-credentials-2.json from Heroku environment variable"
 
 echo $GOOGLE_CREDENTIALS > google-credentials.json
+echo $GOOGLE_CREDENTIALS_2 > google-credentials-2.json
 
 exec "$@"
